3a9fef0dc25b1b65468db66eeda5fa19ea1fdf58
MaironLucas <maironlucaslusarz@gmail.com>
2025-12-05 22:40:42 -0300

    feat: setup dependency injection
    - Add GetIt package to create instances

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,8 +1,16 @@
 import 'dart:async';
 import 'dart:developer';
+import 'dart:io' as io;
 
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:fav_coffee/features/coffee/data/data.dart';
+import 'package:fav_coffee/features/coffee/domain/domain.dart';
+import 'package:file/file.dart';
+import 'package:file/local.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get_it/get_it.dart';
+import 'package:path_provider/path_provider.dart';
 
 class AppBlocObserver extends BlocObserver {
   const AppBlocObserver();
@@ -27,7 +35,31 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
 
   Bloc.observer = const AppBlocObserver();
 
-  // Add cross-flavor configuration here
+  _registerDependencies();
 
   runApp(await builder());
 }
+
+void _registerDependencies() => GetIt.instance
+  ..registerSingleton<Dio>(Dio())
+  ..registerSingleton<CoffeeRemoteDataSource>(
+    CoffeeRemoteDataSource(
+      dio: GetIt.instance<Dio>(),
+    ),
+  )
+  ..registerSingleton<Future<io.Directory> Function()>(
+    getApplicationCacheDirectory,
+  )
+  ..registerSingleton<FileSystem>(const LocalFileSystem())
+  ..registerSingleton<CoffeeLocalDataSource>(
+    CoffeeLocalDataSource(
+      getDirectory: GetIt.instance<Future<io.Directory> Function()>(),
+      fileSystem: GetIt.instance<FileSystem>(),
+    ),
+  )
+  ..registerSingleton<CoffeeRepository>(
+    CoffeeRepository(
+      remoteDataSource: GetIt.instance<CoffeeRemoteDataSource>(),
+      localDataSource: GetIt.instance<CoffeeLocalDataSource>(),
+    ),
+  );
